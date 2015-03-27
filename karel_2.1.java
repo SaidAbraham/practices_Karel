aafb9e3649eda04c4a6b7cf8bbe2f9fe761363df
SaidAbraham <gmm_ss@hotmail.com>
2015-03-26 20:23:07 -0800

    Second Practice

---
+++ karel_2.1.java
@@ -0,0 +1,51 @@
+class program {
+
+program() {
+    iterate (3) {
+    turnleft();
+    }
+
+    iterate (2) {
+    move();
+    }
+    turnleft();
+    iterate (2) {
+    move();
+    }
+    turnleft();
+    iterate (2) {
+    move();
+    }
+
+    turnleft();
+    move();
+    pickbeeper();
+
+    iterate (2) {
+    turnleft();
+    }
+    
+    move();
+
+    iterate (3) {
+    turnleft();
+    }
+
+    iterate (2) {
+    move();
+    }
+
+    iterate (3) {
+    turnleft();
+    }
+
+    iterate (2) {
+    move();
+    }
+
+    iterate (3) {
+    turnleft();
+    }
+
+    iterate (2) {
+    move();
