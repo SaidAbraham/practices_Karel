6e2d03969c8b3d84bb4f50e9d8ce47a4605de457
SaidAbraham <gmm_ss@hotmail.com>
2015-03-30 21:00:06 -0800

    Third Practice

---
+++ karel_3.1.java
@@ -0,0 +1,27 @@
+class program {
+
+program() {
+
+    if((facingWest)) {
+    turnleft();
+    turnleft();
+    turnleft();
+    }
+
+    if((facingEast)) {
+    turnleft();
+    }
+
+    if((facingSouth)) {
+    turnleft();
+    turnleft();
+    }
+
+    if((facingNorth)) {
+    turnoff();
+    }
+    ;
+    turnoff();
+}
+
+}
