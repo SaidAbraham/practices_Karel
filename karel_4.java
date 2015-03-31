f9c728b69d79bcba873c5f00b10bf0770ea777cf
SaidAbraham <gmm_ss@hotmail.com>
2015-03-30 21:25:16 -0800

    Fourth Practice

---
+++ karel_4.java
@@ -0,0 +1,58 @@
+class program {
+
+program() {
+
+    move();
+    iterate (3) {
+    turnleft();
+    }
+    move();
+    pickbeeper();
+    turnleft();
+    move();
+    iterate (3) {
+    turnleft();
+    }
+    move();
+    pickbeeper();
+    turnleft();
+    move();
+    iterate (3) {
+    turnleft();
+    }
+    move();
+    pickbeeper();
+    turnleft();
+    move();
+    iterate (3) {
+    turnleft();
+    }
+    move();
+    pickbeeper();
+    turnleft();
+    move();
+    iterate (3) {
+    turnleft();
+    }
+    move();
+    pickbeeper();
+    turnleft();
+    move();
+    iterate (3) {
+    turnleft();
+    }
+    move();
+    pickbeeper();
+    turnleft();
+    move();
+    iterate (3) {
+    turnleft();
+    }
+    move();
+    pickbeeper();
+    turnleft();
+    ;
+    turnoff();
+}
+
+}
